47e231ce441538ad49f8aacb45f53d0a59c520f4
Teromeh <teromeh@proton.me>
2026-06-29 21:56:58 +0300

    Update Workout data cleaning.R

--- projects/Workout data cleaning.R
+++ projects/Workout data cleaning.R
@@ -8,4 +8,13 @@ workouts <- read.csv("user202919653_workout_history.csv") |>
 
 workouts$workout_date <- mdy(workouts$workout_date)
 
+workouts <- workouts |>
+  mutate(distance_class = case_when(
+    distance_km <= 5 ~ "0-5 km",
+    distance_km <= 10 ~ "5-10 km",
+    distance_km <= 15 ~ "10-15 km",
+    TRUE ~ "15+ km"
+  ))
+
+
 saveRDS(workouts, "workout-history.rds")
