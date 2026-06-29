2737f59f914fd8a79daacffd1468e42f0d820234
Teromeh <teromeh@proton.me>
2026-06-29 16:27:57 +0300

    Donut chart added to the workout data

--- projects/Workout data cleaning.R
+++ projects/Workout data cleaning.R
@@ -1,10 +1,11 @@
 library(dplyr)
 library(janitor)
+library(lubridate)
 
 workouts <- read.csv("user202919653_workout_history.csv") |>
   clean_names() |>
   select(workout_date, activity_type, distance_km)
 
-workouts$workout_date <- as.Date(workouts$workout_date, format = "%B %d, %Y")
+workouts$workout_date <- mdy(workouts$workout_date)
 
 saveRDS(workouts, "workout-history.rds")
