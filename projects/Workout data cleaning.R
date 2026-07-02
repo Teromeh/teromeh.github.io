d7140eaf8c6a02e5495623d95f369b528cb53ac0
Teromeh <271512319+Teromeh@users.noreply.github.com>
2026-07-02 10:44:43 +0300

    Workout charts updated with a year filter

--- projects/Workout data cleaning.R
+++ projects/Workout data cleaning.R
@@ -4,17 +4,14 @@ library(lubridate)
 
 workouts <- read.csv("user202919653_workout_history.csv") |>
   clean_names() |>
-  select(workout_date, activity_type, distance_km)
-
-workouts$workout_date <- mdy(workouts$workout_date)
-
-workouts <- workouts |>
+  select(workout_date, activity_type, distance_km) |>
+  mutate(workout_date = mdy(workout_date)) |>
+  filter(year(workout_date) != 2021) |>
   mutate(distance_class = case_when(
     distance_km <= 5 ~ "0-5 km",
     distance_km <= 10 ~ "5-10 km",
     distance_km <= 15 ~ "10-15 km",
     TRUE ~ "15+ km"
   ))
-
 
 saveRDS(workouts, "workout-history.rds")
